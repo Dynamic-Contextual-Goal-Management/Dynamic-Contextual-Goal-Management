d270e09859083bcf590c61dc3eaf379e53b98866
Dynamic-Contextual-Goal-Management <57679397+Dynamic-Contextual-Goal-Management@users.noreply.github.com>
2020-04-16 15:34:21 +0530

    Add files via upload

---
+++ Tool/Java_Source/goalContext.java
@@ -0,0 +1,22 @@
+package ericsson;
+
+public class goalContext {
+	String goalname,context;
+
+	public String getGoalname() {
+		return goalname;
+	}
+
+	public void setGoalname(String goalname) {
+		this.goalname = goalname;
+	}
+
+	public String getContext() {
+		return context;
+	}
+
+	public void setContext(String context) {
+		this.context = context;
+	}
+	
+}
